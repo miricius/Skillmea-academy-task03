cf57b9e69c25bd8258fa6596f37733d99b92d813
miricius <gmiricius@gmail.com>
2025-05-03 14:14:35 +0200

    Finishing task 03 with <button> element.

--- js/main.js
+++ js/main.js
@@ -44,4 +44,28 @@ function addCard(headingText, paragraphText, addToStart) {
 	}
 }
 
-addCard('Nadpis', 'Text', false);
+// addCard('Nadpis', 'Text', false);
+
+
+// Uloha 03b
+// *********
+
+/*	Vytvorenie formulara, do ktoreho uzivatel zada
+	text pre nadpis a telo karty. Dalej zada, ci
+	sa ma nova karta pridat na zaciatok alebo koniec
+*/
+
+document.getElementById('button').addEventListener('click', function(e) {
+	
+	// Zabranime defaultnemu odoslaniu a reloadu stranky
+	e.preventDefault();
+
+	// Z formulara zistime hodnoty pre nadpis a telo
+	let heading = document.getElementById('heading').value;
+	let paragraph = document.getElementById('paragraph').value;
+
+	// Zistime, ci novu kartu pridat na zaciatok alebo na koniec zoznamu
+	let toStart = document.getElementById('toStart').checked;
+	
+	addCard(heading, paragraph, toStart);
+});
