275f494e3ee53f5b05772f2e26dd74f45f600338
miricius <gmiricius@gmail.com>
2025-05-08 12:30:01 +0200

    Clicking on images - final task_04

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const mitchScore = document.querySelector('.mitch p');
 const vashoPic = document.querySelector('.vasho img');
 const vashoScore = document.querySelector('.vasho p');
 
+// Po kliknuti na obrazok sa zvysi score o 1
 mitchPic.addEventListener('click', () => {
-    console.log('Mitch' + mitchScore.innerText);
+    mitchScore.innerText = Number(mitchScore.innerText) + 1;
 });
-
 vashoPic.addEventListener('click', () => {
-    console.log('Vašo' + vashoScore.innerText);
+    vashoScore.innerText = Number(vashoScore.innerText) + 1;
 });
