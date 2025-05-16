14e8826891e816042bec14baedef170af9df5996
miricius <gmiricius@gmail.com>
2025-05-16 19:02:39 +0200

    Add sum function and Vasho function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // functions
+let array = [];
 
 function fillInArray() {
     const array = [];
@@ -19,5 +20,33 @@ function sum(array) {
 
 
 // MAIN CODE
+array = fillInArray();
+
 // Sum of array
-console.log(`Celkový súčet poľa 0-10 je: ${sum(fillInArray())}`);
+console.log(`Celkový súčet poľa 0-10 je: ${sum(array)}`);
+
+// Max number
+console.log('Najväčšie číslo v poli je: ' + Math.max(...array));
+
+// How many Vashos is in array
+const numberTwo = array.filter(item => item === 1);
+/*
+console.log(numberTwo.length == 0 ? 'V poli nie je žiadny Vašo' :
+    numberTwo.length == 1 ? `V poli je jeden Vašo` :
+    `V poli je ${numberTwo.length} Vašov`);
+*/
+switch (numberTwo.length) {
+    case 0:
+        console.log('V poli nie je žiadny Vašo');
+        break;
+    case 1:
+        console.log('V poli je jeden Vašo');
+        break;
+    case 2:
+    case 3:
+    case 4:
+        console.log(`V poli sú ${numberTwo.length} Vašovia`);
+        break;
+    default:
+        console.log(`V poli je ${numberTwo.length} Vašov`);
+}
